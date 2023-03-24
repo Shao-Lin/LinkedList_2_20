9b679eeff79a8f51c098ca48709745ac0f5616b9
Shao-Lin <baulin2005@bk.ru>
2023-03-24 15:04:18 +0300

    Initial commit

---
+++ src/Task/FrameMain.java
@@ -0,0 +1,16 @@
+package Task;
+
+import javax.swing.*;
+
+public class FrameMain extends javax.swing.JFrame {
+
+
+    private JPanel PanelMain;
+
+    public FrameMain(){
+        this.setTitle("FrameMain");
+        this.setContentPane(PanelMain);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+    }
+}
